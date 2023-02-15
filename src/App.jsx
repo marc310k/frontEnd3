0976b261f236ac1208385f11e15e6e578d4d13c7
marc310k <marcelo.koskur@gmai.com>
2023-02-14 22:15:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+//const App = () => <h1>Meu primeiro componente</h1>;
+
+function App(){
+  return (
+    <main>
+      <div>
+        <h1>Meu primeiro componente</h1>
+        <p>Meu texto</p>
+        <ul>
+          <li>Item 1</li>
+        </ul>
+      </div>
+    </main>
+  )
+
+}
+
+export default App;
